b99c7d6255c899823881f36f324c4c63f9ec4f53
DenGrif <grifon991@gmail.com>
2024-12-10 05:59:56 +0400

    less03_domashka_finish

--- zerocoder/myapp/models.py
+++ zerocoder/myapp/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User  # Импорт модели пользователя
 
 class News_post(models.Model):
 	title = models.CharField('Название новости', max_length=50)
 	short_description = models.CharField('Краткое описание новости', max_length=200)
 	text = models.TextField('Новость')
 	pub_date = models.DateTimeField('Дата публикации')
+	author = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		verbose_name='Автор',
+		related_name='news_posts'
+	)  # Поле для связи с пользователем
 
 	def __str__(self):
 		return self.title
