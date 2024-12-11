7709c0ea527e57af66301d93ef8018bfbe0fc1b8
DenGrif <grifon991@gmail.com>
2024-12-12 00:52:07 +0400

    Polu_FINISH_nado_sdelat_vhod_registr

--- zerocoder/myapp/forms.py
+++ zerocoder/myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import News_post
 
+
 class News_postForm(forms.ModelForm):
     class Meta:
         model = News_post
@@ -9,5 +10,5 @@ class News_postForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите заголовок'}),
             'short_description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите краткое описание'}),
             'text': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите текст новости'}),
-            'pub_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'pub_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
         }
--- zerocoder/myapp/views.py
+++ zerocoder/myapp/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import News_post
 from .forms import News_postForm
 
-
 def home(request):
+    """
+    Главная страница с отображением списка новостей.
+    """
     news = News_post.objects.all()
     context = {
         'caption': 'CatDjango',
@@ -12,7 +14,55 @@ def home(request):
     }
     return render(request, 'myapp/news.html', context)
 
+@login_required
 def create_news(request):
-    form = News_postForm()
-    return render(request, 'myapp/add_new_post.html', {'form':form})
+    """
+    Страница для добавления новостей.
+    Только аутентифицированные пользователи могут добавлять новости.
+    """
+    error = ""
+    if request.method == 'POST':
+        form = News_postForm(request.POST)
+        if form.is_valid():
+            # Создаем объект, но пока не сохраняем его в базу
+            news = form.save(commit=False)
+            # Указываем текущего пользователя как автора
+            news.author = request.user
+            news.save()  # Сохраняем объект с автором
+            return redirect('news_home')  # Перенаправляем на главную страницу
+        else:
+            error = "Данные были заполнены некорректно"
+    else:
+        form = News_postForm()
+    return render(request, 'myapp/add_new_post.html', {'form': form, 'error': error})
 
+
+
+# from mmap import error
+#
+# from django.http import HttpResponse
+# from django.shortcuts import render, redirect
+# from .models import News_post
+# from .forms import News_postForm
+#
+#
+# def home(request):
+#     news = News_post.objects.all()
+#     context = {
+#         'caption': 'CatDjango',
+#         'news': news
+#     }
+#     return render(request, 'myapp/news.html', context)
+#
+# def create_news(request):
+#     error = ""
+#     if request.method == 'POST':
+#         form = News_postForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('news_home')
+#         else:
+#             error = "Данные были заполнены некорректно"
+#     form = News_postForm()
+#     return render(request, 'myapp/add_new_post.html', {'form':form, 'errors': error})
+#
